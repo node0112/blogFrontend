0564a6ce22c75bad280c39ffc70f527bffe560f2
Sid <96074317+node0112@users.noreply.github.com>
2023-05-05 22:29:52 +0100

    completed post page

--- src/components/PostPage.jsx
+++ src/components/PostPage.jsx
@@ -4,6 +4,7 @@ import postAPI from './postAPI'
 import { refreshAcessToken } from './postAPI';
 import parse from 'html-react-parser';
 import './css/postPage.css'
+import Loading from './Loading';
 
 function PostPage({postID}) {
   let navigate = useNavigate()
@@ -12,6 +13,9 @@ function PostPage({postID}) {
   const [postTitle,setPostTitle] = useState('')
   const [postAuthor,setPostAuthor] = useState('')
   const [postLikes,setLikes] = useState('')
+  const [postDate,setPostDate] = useState('')
+  const [loading,setLoading] = useState(false)
+
   const [postComments,setPostComments] = useState('')
 
   useEffect(()=>{
@@ -21,10 +25,12 @@ function PostPage({postID}) {
     if(postID === '' ){
       navigate('/') //incase a user stumbles here bby accident
     }
+    setLoading(true)
     postAPI.get('/post/'+postID).then(resData=>{
       let data = resData.data
       let errors = []
       if(data.errors) errors = data.errors;
+      setLoading(false)
       if(errors.length > 0){ //refresh token if it has expired
         if(errors[0] === 'jwt expired' || errors[0] === 'jwt malformed'){ //if errors in the token
           refreshAcessToken().then(fetchPost)
@@ -45,14 +51,15 @@ function PostPage({postID}) {
     setPostTitle(post.title)
     setPostAuthor(post.author)
     setLikes(post.likes)
+    setPostDate(post.date.substring(0,10))
     setParsedPost(parsedHtml)
-
     document.querySelector('.main-post-author').style.color = post.textColor
   }
 
 
   return (
     <div className='main-post flex column'>
+      <Loading loading={loading} />
       <div className="main-post-title">{postTitle}</div>
       <div className="post-info-container flex">
         <div className="main-post-author">By {postAuthor}</div>
@@ -62,6 +69,7 @@ function PostPage({postID}) {
         </div>
         <hr className="sep-line" />
       </div>
+      <div className="date">{postDate}</div>
       <div>{parse(parsedPost)}</div>
     </div>
   )
